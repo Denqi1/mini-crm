79d232cf8fa28caabc2ba66509d4937abe9eeb38
Denqi1 <denqilen@gmail.com>
2026-06-09 17:47:39 +0300

    feat: add fonts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,56 +1,18 @@
-import { useEffect, useState } from 'react';
-
-import { useDebounce } from '../shared/lib/useDebounce';
-import { getContacts } from '../entities/contacts';
-
 import './App.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(null);
-  const [searchValue, setSearchValue] = useState('');
-
-  const debouncedValue = useDebounce(searchValue);
-
-  const handleSearchValueChange = (e) => {
-    setSearchValue(e.target.value);
-  };
-
-  useEffect(() => {
-    const makeRequest = async () => {
-      const contactsResponse = await getContacts();
-
-      setContacts(contactsResponse);
-    };
-
-    makeRequest();
-  }, []);
-
-  const filteredContacts = contacts?.filter((contact) => {
-    const { name } = contact;
-
-    const nameLowered = name.toLowerCase();
-    const searchValueLowered = debouncedValue.toLowerCase();
-
-    return nameLowered.includes(searchValueLowered);
-  });
-
   return (
-    <>
-      <input
-        placeholder="фильтрация по имени"
-        value={searchValue}
-        onChange={handleSearchValueChange}
-      />
-
-      {filteredContacts?.map((contact) => {
-        return (
-          <div key={contact.id} style={{ backgroundColor: 'gray' }}>
-            <p>name: {contact.name}</p>
-            <p>email: {contact.email}</p>
-            <p>company: {contact.company}</p>
-          </div>
-        );
-      })}
-    </>
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga repellendus facere earum in
+        aliquid itaque quia deleniti, iure, cum pariatur ex placeat sit est, ab nisi odit veniam
+        quis quasi! Ea neque, inventore accusantium libero obcaecati id quisquam corporis incidunt
+        autem reprehenderit non aspernatur, voluptates aperiam hic magnam excepturi pariatur soluta
+        minus ipsa error, ipsum fuga animi modi. Laborum, molestias. Voluptas magni quia
+        exercitationem cupiditate, nemo dicta dolores animi recusandae dolor commodi quasi
+        reiciendis deleniti, iste ut neque nam odio dignissimos, praesentium ratione expedita nihil
+        molestias. Voluptatum fuga velit saepe.
+      </p>
+    </div>
   );
 };

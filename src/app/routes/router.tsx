af0656fa30aa6f517a558d3308da5dc233d3b09c
Denqi1 <denqilen@gmail.com>
2026-06-09 23:08:55 +0300

    feat: add contacts page

---
+++ src/app/routes/router.tsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+import { ContactsPage } from "../../pages/Contacts";
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <ContactsPage />
+  }
+])
--- src/entities/contacts/api/index.ts
+++
@@ -1,1 +0,0 @@
-export { getContacts } from './getContacts';

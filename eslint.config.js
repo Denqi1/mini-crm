cb46411184110c7b69d38b79d0ab94025968887d
Denis Skorik <denqilen@gmail.com>
2026-06-10 14:14:03 +0300

    Add a rule to organize imports and aliases (#8)
    * chore: add import alias and import sort
    
    * docs: add an explanatory comment for the `ignoreDeprecations` option; add dots to the missing JSDOC
    
    * chore: change the `printWidth` rule to 80 characters in Prettier and add the rule to ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,5 +19,60 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            /**
+             * React and third-party.
+             */
+            ['^react$', '^\\w'],
+            /**
+             * Other packages.
+             */
+            ['^@?\\w'],
+            /**
+             * Internal path aliases (FSD layers).
+             */
+            ['^@/app'],
+            ['^@/pages'],
+            ['^@/widgets'],
+            ['^@/features'],
+            ['^@/entities'],
+            ['^@/shared'],
+            /**
+             * Local relative imports.
+             */
+            ['^\\.'],
+            /**
+             * Styles.
+             */
+            ['\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+
+      'max-len': [
+        'error',
+        {
+          code: 80,
+          comments: 80,
+        },
+      ],
+    },
   },
-])
+]);
